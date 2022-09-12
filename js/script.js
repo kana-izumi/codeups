fb88d417e3915e2b59c36f3ad19d2e6bda416dc0
kana-izumi <xxkn08@gmail.com>
2022-09-13 04:40:16 +0900

    0913

--- js/script.js
+++ js/script.js
@@ -48,3 +48,19 @@ jQuery(function ($) { // この中であればWordpressでも「$」が使用可
   });
 
 });
+
+// mvスワイパー
+let swipeOption = {
+  loop: true,
+  effect: 'fade',
+  autoplay: {
+    delay: 4000,
+    disableOnInteraction: false,
+  },
+  speed: 2000,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+  }
+}
+new Swiper('.swiper-container', swipeOption);
